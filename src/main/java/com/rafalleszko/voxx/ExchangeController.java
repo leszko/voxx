6e67a665350b529d2ef88d1bf9fd077cc6fedf53
leszko <rafal.leszko@gmail.com>
2016-10-31 22:44:57 +0100

    Introduce a bug.

--- src/main/java/com/rafalleszko/voxx/ExchangeController.java
+++ src/main/java/com/rafalleszko/voxx/ExchangeController.java
@@ -12,7 +12,7 @@ public class ExchangeController {
             method = RequestMethod.GET)
     public String exchangeRate(@PathVariable String currency) {
         if ("usd".equalsIgnoreCase(currency)) {
-            return "1.00";
+            return "2.00";
         } else {
             return "not supported";
         }
